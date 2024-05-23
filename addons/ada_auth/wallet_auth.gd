d030e08fe1355bace3370c8f3149b7f71273bedc
Den <den@Deniss-MacBook-Pro.local>
2024-05-23 12:17:21 +0200

    disconnect btn, refac http handling, load assets on start

--- addons/ada_auth/wallet_auth.gd
+++ addons/ada_auth/wallet_auth.gd
@@ -2,8 +2,12 @@
 class_name WalletAuth
 extends Node
 
-const API_URL = "http://localhost:3000/api";
-const AUTH_URL = "http://localhost:3000/auth";
+#const BASE_URL = "https://unfra.me";
+const BASE_URL = "http://localhost:3000";
+
+const API_URL = BASE_URL + "/api";
+const AUTH_URL = BASE_URL + "/auth";
+const JWT_PATH = "user://jwt.dat";
 
 var handshake: String = "";
 var poll_timer: Timer;
@@ -11,19 +15,27 @@ var handshake_request: HTTPRequest;
 var polling_request: HTTPRequest;
 var project_key: String;
 var assets: Array;
+#var isLoggedIn: bool = false;
 
 signal success;
 signal loaded;
-#signal error;
+signal logout;
+signal error;
 
 func init(_project_key: String):
 	project_key = _project_key;
+	init_user_state();
 	return self;
+	
+func reset():
+	assets.clear();
+	delete_jwt();
+	emit_signal("logout");
 	
 func _ready():
 	handshake_request = HTTPRequest.new();
 	add_child(handshake_request);
-	handshake_request.request_completed.connect(_on_handshake_completed)
+	handshake_request.request_completed.connect(_on_handshake_completed);
 
 func cancel():
 	handshake = "";
@@ -38,8 +50,8 @@ func start():
 		print("Error: handshake_request is not initialized")
 	
 func _on_handshake_completed(result, response_code, headers, body):
-	var json = JSON.parse_string(body.get_string_from_utf8())
-	print(json.handshake)
+	var json = handle_http_response(result, response_code, headers, body)
+	print(json)
 	handshake = json.handshake
 	navigate_to_auth(json.handshake)
 
@@ -68,7 +80,7 @@ func poll():
 	
 func _on_poll_completed(result, response_code, headers, body):
 	print("polled")
-	var json = JSON.parse_string(body.get_string_from_utf8())
+	var json = handle_http_response(result, response_code, headers, body)
 	print(json)
 	if json and json["token"]["verified"]:
 		print('VERIFIED !')
@@ -77,23 +89,32 @@ func _on_poll_completed(result, response_code, headers, body):
 		#$CancelButton.hide()
 		save_jwt(json.token.jwt)
 		emit_signal('success')
-		print("load", load_jwt())
+		load_assets()
+		#print("load", load_jwt())
 
 func save_jwt(jwt: String):
-	var file = FileAccess.open("user://jwt.dat", FileAccess.WRITE)
+	var file = FileAccess.open(JWT_PATH, FileAccess.WRITE)
 	file.store_string(jwt)
+	file.close()
 
 func load_jwt():
-	var file = FileAccess.open("user://jwt.dat", FileAccess.READ)
-	var jwt = file.get_as_text()
-	return jwt;
+	var file = FileAccess.open(JWT_PATH, FileAccess.READ)
+	if file:
+		var jwt = file.get_as_text()
+		file.close()
+		return jwt;
+	else:
+		return null;
+
+func delete_jwt():
+	DirAccess.remove_absolute(JWT_PATH);
 
 func load_assets():
 	var jwt = load_jwt();
 	var assets_request = HTTPRequest.new();
 	add_child(assets_request);
 	assets_request.request_completed.connect(_on_assets_loaded)
-	
+	print(jwt)
 	var headers = [
 		"Content-Type: application/json",
 		"Authorization: Bearer " + jwt
@@ -102,7 +123,45 @@ func load_assets():
 	assets_request.request(API_URL + "/assets", headers)
 
 func _on_assets_loaded(result, response_code, headers, body):
+	var json = handle_http_response(result, response_code, headers, body)
+	#print(json)
+	if json:
+		assets = json.get("assets")
+		print("Loaded ", assets.size(), " assets")
+		emit_signal('loaded', assets)
+	else:
+		print('wtf')
+func init_user_state():
+	if load_jwt():
+		#isLoggedIn = true;
+		load_assets();
+	#else:
+		#isLoggedIn = false;
+
+func handle_http_response(result, response_code, headers, body):
+	print(result)
+	print(response_code)
+	#print(headers)
+	var error;
+	
+	if response_code >= 400:
+		error = "Request error"
+
 	var json = JSON.parse_string(body.get_string_from_utf8())
-	assets = json.assets
-	print("Loaded ", assets.size(), " assets")
-	emit_signal('loaded', assets)
+	#print(json)
+	if json:
+		if json.has("error"):
+			error = json.error;
+		if json.has("message"):
+			error = json.message;
+	else:
+		error = "Unexpected JSON format.";
+		
+	if error:
+		print("handle - error")
+		emit_signal('error', error)
+		return null;
+	else:
+		print("handle - json")
+		return json;
+
--- connect.gd
+++ connect.gd
@@ -12,10 +12,10 @@ extends Node2D
 
 @onready var label: Label = $Control/CenterContainer/VBoxContainer/Label
 @onready var connect_button: Button = $Control/CenterContainer/VBoxContainer/ConnectButton
-@onready var load_button: Button = $Control/CenterContainer/VBoxContainer/LoadButton
+#@onready var load_button: Button = $Control/CenterContainer/VBoxContainer/LoadButton
 @onready var cancel_button: Button = $Control/CenterContainer/VBoxContainer/CancelButton
 @onready var item_list: ItemList = $Control/CenterContainer/VBoxContainer/ItemList
-
+@onready var disconnect_button: Button = $Control/CenterContainer/VBoxContainer/DisconnectButton
 
 const API_URL = "http://localhost:3000/api";
 const AUTH_URL = "http://localhost:3000/auth";
@@ -28,17 +28,25 @@ var handshake_request;
 func _ready() -> void:
 	auth.connect("success", _on_auth_success)
 	auth.connect("loaded", _on_load_success)
+	auth.connect("error", _on_auth_error)
+	auth.connect("logout", _on_logout)
 	connect_button.connect("pressed", _on_connect_pressed)
 	cancel_button.connect("pressed", _on_cancel_pressed)
-	load_button.connect("pressed", _on_load_pressed)
+	disconnect_button.connect("pressed", _on_disconnect_pressed)
+	#load_button.connect("pressed", _on_load_pressed)
 	cancel_button.hide()
-	label.text = "Log in with your wallet"
+	label.text = "Connect your wallet"
 	
 func _on_connect_pressed() -> void:
 	auth.start()
 	connect_button.hide()
 	cancel_button.show()
 	label.text = "Waiting for auth..."
+
+func _on_disconnect_pressed() -> void:
+	auth.reset()
+	connect_button.show()
+	label.text = "Log in with your wallet"
 	
 func _on_cancel_pressed() -> void:
 	auth.cancel();
@@ -48,21 +56,39 @@ func _on_cancel_pressed() -> void:
 	
 func _on_auth_success() -> void:
 	cancel_button.hide()
-	load_button.show()
+	#load_button.show()
 	label.text = "Verified !"
 
 func _on_load_pressed() -> void:
 	auth.load_assets()
 	label.text = "Loading assets..."
-	load_button.hide()
+	#load_button.hide()
 	
 func _on_load_success(assets: Array) -> void:
 	label.text = "Loaded " + str(assets.size()) + " assets!"
 	item_list.show()
+	disconnect_button.show()
+	connect_button.hide()
 	for asset in assets:
+		#print(asset.onchain_metadata)
 		if asset and asset.onchain_metadata:
 			if "name" in asset.onchain_metadata:
 				item_list.add_item(asset.onchain_metadata["name"])
 			elif "title" in asset.onchain_metadata:
 				item_list.add_item(asset.onchain_metadata["title"])
-			
+
+func _on_auth_error(error) -> void:
+	print(error)
+	label.text = error;
+	
+func _on_logout() -> void:
+	item_list.clear()
+	item_list.hide()
+	connect_button.show();
+	disconnect_button.hide()
+	label.text = "Connect with your wallet";
+	
+#func _on_loading() -> void:
+	#print(2)
+	#connect_button.hide()
+	#label.text = "Connecting...";
